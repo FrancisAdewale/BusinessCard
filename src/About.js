59fa525c822ae76b3c391c83ea6bdc6aef2fb608
Francis Adewale <francisadewale1992@gmail.com>
2022-02-14 12:07:24 +0000

    About component complete

---
+++ src/About.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function About() {
+    return (
+        <>
+            <h1 className="about--title">About</h1>
+            <p className="about--details">I am a self taught software developer
+            with a particular interest in web development and mobile development.
+            I have more personal experience in mobile app development but have since transitioned
+            onto web development, concentrating on full stack. 
+            </p>
+        </>
+    )
+
+}
